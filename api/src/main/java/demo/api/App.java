17bcc14b4f241f89d7bc9fe316ca3b59e7613567
HP <pavelveka100@gmail.com>
2021-01-13 11:48:44 +0300

    fixed code

---
+++ api/src/main/java/demo/api/App.java
@@ -0,0 +1,8 @@
+package demo.api;
+
+public class App {
+    public static void main(String[] args) {
+        demo.core.Utils utils = new demo.core.Utils();
+        System.out.println(utils.isAllPositiveNumbers("12", "79"));
+    }
+}
